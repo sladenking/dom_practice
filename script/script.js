120e8036c042f3c2dcd0f8949b96ed6b052ff272
Viacheslav Denysko <deniskosv@gmail.com>
2020-04-14 22:46:34 +0300

    done

--- script/script.js
+++ script/script.js
@@ -2,8 +2,34 @@
 
 function DomElemement(selector, height, width, bg, fontSize) {
     this.selector = selector;
-    this. height = height;
-    this. width = width;
-    this. bg = bg;
-    this. fontSize = fontSize;    
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;    
 }
+
+DomElemement.prototype.selectorAdd = function() {
+    if (this.selector.slice(0,1) === '.'){
+        const div = document.createElement('div');
+        div.textContent="div";
+        div.classList.add(this.selector.slice(1));
+        div.style.cssText = `height: ${this.height}px; width: ${this.width}px; 
+        background-color: ${this.bg}; font-size: ${this.fontSize}px`;
+        document.body.append(div);
+    }
+    if (this.selector.slice(0,1) === '#'){
+        const p = document.createElement('p');
+        p.textContent="p";
+        p.id=this.selector.slice(1);
+        p.style.cssText = `height: ${this.height}px; width: ${this.width}px; 
+        background-color: ${this.bg}; font-size: ${this.fontSize}px`;
+        document.body.append(p);
+    }  
+};
+
+let p = new DomElemement('#paragraph', 100, 200, '#bbb', 20);
+let div = new DomElemement('.block', 100, 200, '#47d147', 20);
+
+
+p.selectorAdd();
+div.selectorAdd();
