e2f9aa68b78844568bca68b3ec7b7e39fed0ce59
Viacheslav Denysko <deniskosv@gmail.com>
2020-04-14 12:49:53 +0300

    created started files

---
+++ script/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function DomElemement(selector, height, width, bg, fontSize) {
+    this.selector = selector;
+    this. height = height;
+    this. width = width;
+    this. bg = bg;
+    this. fontSize = fontSize;    
+}
